7581575d4985de1af28c07b191c3d87190a9be2b
Abhishek kumawat <93272330+abhi0a11@users.noreply.github.com>
2022-12-17 02:58:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// // Targeting video element 
+// let clip = document.querySelector(".vid-1")
+// // let link=document.getElementById("vid_box_1").innerHTML= `<a  data-aos="flip-up" href="https://abhi0a11.github.io/lt7.00/" target="_blank">`;
+// /* Applying mouseover event on video clip 
+// and then we call play() function to play 
+// the video when the mouse is over the video */
+// clip.addEventListener("mouseover", function (e) {
+//     clip.play();
+// })
+
+// /* Applying mouseout event on video clip 
+// and then we call pause() function to stop 
+// the video when the mouse is out the video */
+// clip.addEventListener("mouseout", function (e) {
+//     clip.pause();
+// })
+
